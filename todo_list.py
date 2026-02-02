31fbb4eed9dbdd834e33d6b8e970b96c5801b896
Andrei Gurd <Andreigurd@gmail.com>
2026-02-01 22:38:14 -0500

    created main program with menu and load/save JSON

---
+++ todo_list.py
@@ -0,0 +1,77 @@
+import json
+from tabulate import tabulate
+from datetime import datetime,date,timedelta
+#-----------------------------------------------------------------------
+#   opening expenses json file
+#-----------------------------------------------------------------------
+try:
+    with open('tasks.json', 'r') as file:
+        contacts = json.load(file)
+except FileNotFoundError:
+    print("Contacts file not found. Blank list created.")
+    contacts = [] # makes an empty list
+except json.JSONDecodeError:
+    print("Issue loading Contacts file. File empty or invalid JSON file. Blank Contacts list created.")
+    contacts = []
+except ValueError:
+    print("Invalid Contact item. Blank list created.")
+    contacts = []
+except PermissionError:
+    print("Need permission to access Contacts file. Blank Contacts list created.")
+    contacts = []
+
+
+#-----------------------------------------------------------------------
+#   showing menu
+#-----------------------------------------------------------------------
+
+def show_menu():    
+    print("[0] Exit")
+    print("[1] Add task")
+    print("[2] View all tasks")
+    print("[3] View by priority")
+    print("[4] View by status")
+    print("[5] Mark complete")
+    print("[6] Delete task")   
+
+
+#-----------------------------------------------------------------------
+#   option [1] Add Contact
+#-----------------------------------------------------------------------
+
+#-----------------------------------------------------------------------
+#   function to write to contacts json
+#-----------------------------------------------------------------------
+def write_json():
+    with open('tasks.json', 'w') as file:
+        json.dump(contacts, file, indent=4)
+        
+
+#-----------------------------------------------------------------------
+#   while loop to get user input
+#-----------------------------------------------------------------------
+
+while True:
+    show_menu()
+    option = input("\nSelect Option: ")
+    if option == '0':        
+        write_json()
+        print("Goodbye.")
+        break    
+    elif option == '1':
+        add_task()
+        write_json()
+    elif option == '2':
+        view_tasks()
+    elif option == '3': 
+        view_priority()
+    elif option == '4': 
+        view_status()
+    elif option == '5': 
+        mark_complete()    
+    elif option == '6':
+        delete_contact()
+        write_json     
+
+    else:
+        print("Invalid action. Please try again.")
